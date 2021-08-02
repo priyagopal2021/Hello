85da37536f29a8d1000f79eaa1ad4f6eeac517f6
priyagopal2021 <priyadarshinigopal8287@gmail.com>
2021-08-02 15:40:03 +0530

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.movieapplication {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires javafx.graphics;
+
+    opens com.example.movieapplication to javafx.fxml;
+    exports com.example.movieapplication;
+}
